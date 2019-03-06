7f6098bf42bb1765633d626aabc3a4cf46e04cb4
Sucre <939067012@qq.com>
2019-03-06 18:27:08 +0800

    Update VolatileBank.java
    f

--- src/main/java/com/runforlove/synchronousresources/demo/VolatileBank.java
+++ src/main/java/com/runforlove/synchronousresources/demo/VolatileBank.java
@@ -2,6 +2,7 @@ package com.runforlove.synchronousresources.demo;
 
 public class VolatileBank implements Bank {
 	
+        // 保证了可见性，不保证原子性，有写操作的场景，使用它，对资源同步无效；
 	private volatile Double money;
 	
 	@Override
